255f5b73354e597542cdc1bad1e6bd43da8bd7e1
Deepthi1024ai <morlaposisrideepthi@gmail.com>
2025-10-16 10:54:35 +0530

    Implement class with pointer manipulation in C++

---
+++ use pointers to access and manipulate objects  of a class..cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+#include <string> 
+using namespace std; 
+class student 
+{ 
+private: 
+    string name; 
+    int age; 
+public: 
+    student(string n = "no name", int a = 0) 
+    { 
+        name = n; 
+        age = a; 
+    } 
+    void setplayer(string n) 
+    { 
+        name = n; 
+    } 
+    void dispInfo() 
+    { 
+        cout << "Name: " << name << " Age: " << age << endl; 
+    } 
+}; 
+int main() 
+{ 
+    student s("Deepthi", 17);\ 
+    student* sp; 
+    sp = &s; 
+    sp->dispInfo(); 
+    sp->setplayer("Deepthi"); 
+    sp->dispInfo(); 
+    student* sp1 = new student("M.Deepthi", 18); 
+    sp1->dispInfo(); 
+    delete sp1; 
+    return 0; 
+}
